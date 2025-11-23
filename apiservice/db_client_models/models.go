96c8a39641e235d2433884f1918784e10977a502
Stepan <lskuku1211@gmail.com>
2025-11-23 17:23:00 +0300

    db client models

---
+++ apiservice/db_client_models/models.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Text       string    `json:"text"`
+	CreateTime time.Time `json:"create_time"`
+	Complete   bool      `json:"complete"`
+	CompleteAt time.Time `json:"complete_at"`
+}
+
+type CreateTaskRequest struct {
+	Name string `json:"name"`
+	Text string `json:"text"`
+}
